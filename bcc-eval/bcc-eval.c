9ee2f599fa7dde5841ebcdb4d8953c2e04084888
Hunter Allen <allen286@purdue.edu>
2016-04-27 18:02:18 -0400

    Remote BCC compilation and execution now works.

--- bcc-eval/bcc-eval.c
+++ bcc-eval/bcc-eval.c
@@ -2,6 +2,7 @@
 #include <sys/socket.h>
 #include <sys/signal.h>
 #include <sys/types.h>
+#include <sys/ioctl.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -23,9 +24,18 @@
 #define true  1
 #define false 0
 
+const char * _good_color = "\x1b[35;1m"; // <-- That's racist.
+const char *  _err_color = "\x1b[31;1m";
+const char *  _std_color = "\x1b[0m";
+
 const unsigned short _queue_length = 5;
 
+const char * OK  = "\x1b[34;1m[ \x1b[32;1mok \x1b[34;1m]\x1b[0m";
+const char * ERR = "\x1b[34;1m[ \x1b[33;1merr \x1b[34;1m]\x1b[0m";
+
 void process_request(int fd);
+void printOk();
+void printErr();
 
 void sigchld_handlr(int signum)
 { for(int pid = -1; pid = waitpid(-1, NULL, WNOHANG) > 0;); }
@@ -76,15 +86,38 @@ int main(int argc, char ** argv)
     ssize_t alen = sizeof(client_ip);
     int slave = accept(masterSocket, (struct sockaddr*) &client_ip,
 		       (socklen_t*) &alen);
-    D fprintf(stderr, "received client connection!\n");
     if (slave < 0) {
       perror("accept");
       _exit(4);
     }
-
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+  
+    printOk("client connection received");
     process_request(slave);
-    close(slave);
   } return 0;
+}
+
+void printOk(const char * msg) {
+  size_t len = strlen(msg) + strlen("[ ok ]");
+  fprintf(stderr, _good_color);
+  struct winsize w;
+  ioctl(1,TIOCGWINSZ, &w);
+  fprintf(stderr, msg);
+  for(; w.ws_col - len++; fprintf(stderr, " "));
+  fprintf(stderr, OK);
+  fprintf(stderr, "\x1b[0m\n");
+}
+
+void printErr(const char * msg) {
+  size_t len = strlen(msg) + strlen("[ err ]");
+  fprintf(stderr, _err_color);
+  struct winsize w;
+  ioctl(1,TIOCGWINSZ, &w);
+  fprintf(stderr, msg);
+  for(; w.ws_col - len++; fprintf(stderr, " "));
+  fprintf(stderr, ERR);
+  fprintf(stdout, "\x1b[0m\n");
 }
 
 /**
@@ -105,8 +138,6 @@ void process_request(int fd)
   unsigned char next_char = '\0';
   unsigned char last_char = '\0';
 
-  int state = 0;
-
   int pid = -1;
   
   for (int n = 0; request_len < max_request && (n = read(fd, &next_char,
@@ -120,31 +151,81 @@ void process_request(int fd)
       if (request_len) last_char = _get_request[request_len-1];
       _get_request[request_len++] = next_char;
     }
-  } D fprintf(stderr, "I read: \"%s\"\n", _get_request); 
-     
-  FILE * _fd;
-  if (!(_fd = fopen("out.bc", "w"))) {
-    perror("fopen");
   }
-  fprintf(_fd, "%s", _get_request);
-  fclose(_fd); free(_get_request);
+  char * copy = _get_request;
+  pid = fork();
 
-  /** Compile the now saved source! **/
-  int tmpin  = dup(0);
+  if (pid == 0) {
+    int fdpipe[2]; int pid2;
+    if (pipe(fdpipe) < 0) {
+      printErr("failed to construct pipe");
+      _exit(8);
+    } else if ((pid2 = fork()) < 0) {
+      printErr("failed to fork process");
+      _exit(9);
+    } else if (pid2 == 0) {
+      dup2(fdpipe[0], 0);
+      dup2(2, fd);// write errors to host
+      close(fdpipe[0]); close(1);
+      close(fdpipe[1]);
+      if (execlp("bcc", "bcc", "out.s", NULL)) {
+	printErr("bcc failed -- is it installed?");
+      }
+    } else {
+      close(fdpipe[0]);
+      write(fdpipe[1], _get_request, request_len);
+      close(fdpipe[1]);
+      
+      waitpid(pid2, NULL, 0);
+      char * buff = (char*) calloc(1024, sizeof(char));
+      sprintf(buff, "process [%d] (bcc) has exited.", pid2);
+      printOk(buff); free(buff);
+      _exit(0);
+    }
+  } else if (pid > 0) {
+    struct winsize w;
+    ioctl(1,TIOCGWINSZ, &w);
+    waitpid(pid, NULL, 0);
+    char * buff = (char*) calloc(1024, sizeof(char));
+    sprintf(buff, "process [%d] has exited.", pid);
+    printOk(buff); free(buff);
+  } free(copy);
 
-  int fdin = open("out.bc", O_WRONLY, 0655);
-  if (fdin < 0) {
-    perror("input file");
-    _exit(6);
+  /** generate out.txt **/
+  if (write(fd, "\n", 1) < 0) {
+    printErr("Client disconnected unexpectedly.");
+    return;
   }
 
-  // dup server code to stdin
-  dup2(fdin, 0);
-  close(fdin);
+  int fdpipe[2];
+  if (pipe(fdpipe) < 0) {
+    printErr("Failed to construct pipe.");
+    close(fd); return;
+  } else if ((pid = fork()) < 0) {
+    printErr("Failed to fork process");
+    close(fd); return;
+  } else if (pid == 0) {
+    close(fdpipe[0]);
 
-  execlp("bcc", "bcc", "out.s", (char*) NULL);
-  
-  /** Restore I/O Default **/
-  dup2(tmpin, 0);
-  close(tmpin);
+    dup2(fdpipe[1], 1); // stdout to pipe
+    dup2(fdpipe[1], 2); // stderr to pipe
+
+    close(fdpipe[1]);
+
+    execlp("./out", "./out", NULL);
+    _exit(0);
+  } else {
+    char buff[2048]; memset(buff, 0, 2048);
+    close(fdpipe[1]);
+
+    for(;read(fdpipe[0], buff, sizeof(buff)););
+    waitpid(pid, NULL, 0);
+    char buff2[64]; memset(buff2, 0, 64);
+    sprintf(buff2, "process [%d] has exited", pid);
+    printOk(buff2);
+    /** Send output to client **/
+    D fprintf(stderr, "bufflen: %zd\n", strlen(buff));
+    for (char * b = buff; *b && write(fd, b++, 1); b - buff != strlen(buff));
+    printOk("closing client connection.");
+  }
 }
